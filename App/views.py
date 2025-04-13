4316140d4dbbdc8fe610eb2c592b9d0f5cbb26ae
Fakhrul Islam <fahad.aust09@gmail.com>
2025-04-13 21:14:32 +0600

    celery added

--- App/views.py
+++ App/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .task import test_func
 
 
 class TestApi(APIView):
     def get(self, request):
         print("ok----------")
+        test_func.delay()
         return Response(data={
             "name": "fahadsssssssssssssdfasedfs"
         },status=status.HTTP_200_OK)
